293f593258872889708cd4b82546ca9967d265d8
Nat-crit20 <greuelnathan74@gmail.com>
2024-08-08 11:16:10 -0500

    Created the link token post method

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const { Configuration, PlaidApi, PlaidEnvironments } = require("plaid");
 const app = express();
 const PORT = 3000;
 const PLAID_ENV = process.env.PLAID_ENV || "sandbox";
+const PLAID_PROD = process.env.PLAID_PRODUCTS;
+const PLAID_COUNTRY_CODES = process.env.PLAID_COUNTRY_CODES;
 app.use(express.json());
 
 const config = new Configuration({
@@ -17,6 +19,25 @@ const config = new Configuration({
 const client = new PlaidApi(config);
 app.get("/", (request, response) => {
   response.json("Hello World");
+});
+
+app.post("/api/create_link_token", async (request, response, next) => {
+  const configToken = {
+    user: {
+      client_user_id: "client_id",
+    },
+    client_name: "Bank App",
+    products: PLAID_PROD,
+    language: "en",
+    country_codes: "PLAID_COUNTRY_CODES",
+  };
+  try {
+    const createTokenResponse = await client.linkTokenCreate(configToken);
+    response.json(createTokenResponse.data);
+  } catch (error) {
+    console.log(`Error getting link token: ${error}`);
+    next;
+  }
 });
 app.listen(PORT, () => {
   console.log(`Listening on Port ${PORT}`);
