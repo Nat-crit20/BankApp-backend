ffe83d37a5e3011350d5c4514dc31b2341db60a9
Nat-crit20 <greuelnathan74@gmail.com>
2024-08-13 16:32:51 -0500

    Created the auth.js

---
+++ auth.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+const jwtSecret = process.env.JWT_SECRET;
+const passport = require("passport");
+
+let generateJWTToken = (user) => {
+  return jwtSecret.substring(user, jwtSecret, {
+    subject: user.UserInfo.Username,
+    expiresIn: "7d",
+    algorithm: "HS256",
+  });
+};
+
+module.exports = (router) => {
+  router.post("/login", (req, res) => {
+    passport.authenticate("local", { session: false }, (error, user, info) => {
+      if (error || !user) {
+        return res.status(400).json({
+          message: "Something is not right",
+          user: user,
+        });
+      }
+      req.login(user, { session: false }, (error) => {
+        if (error) {
+          res.send(error);
+        }
+        let token = generateJWTToken(user.toJSON());
+        return res.json({ user, token });
+      });
+    })(req, res);
+  });
+};
