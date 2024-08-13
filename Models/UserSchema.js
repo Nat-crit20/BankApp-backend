7a4652e1fae3bdf570de13516d5019691f82e035
Nat-crit20 <greuelnathan74@gmail.com>
2024-08-13 16:35:10 -0500

    Created the passport.js

--- Models/UserSchema.js
+++ Models/UserSchema.js
@@ -1,19 +1,17 @@
 const mongoose = require("mongoose");
 
 let userSchema = mongoose.Schema({
-  UserInfo: {
-    Username: {
-      type: String,
-      required: true,
-    },
-    Password: {
-      type: String,
-      requited: true,
-    },
-    Email: {
-      type: String,
-      required: true,
-    },
+  Username: {
+    type: String,
+    required: true,
+  },
+  Password: {
+    type: String,
+    requited: true,
+  },
+  Email: {
+    type: String,
+    required: true,
   },
   Access_token: {
     type: String,
