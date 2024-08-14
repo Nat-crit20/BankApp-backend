a4c8a1fd93090eebfaa6a9513e4a137e55fe47c0
Nat-crit20 <greuelnathan74@gmail.com>
2024-08-14 08:55:55 -0500

    Created password hashing

--- Models/UserSchema.js
+++ Models/UserSchema.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 let userSchema = mongoose.Schema({
   Username: {
@@ -18,6 +19,14 @@ let userSchema = mongoose.Schema({
     required: false,
   },
 });
+
+userSchema.statics.hashPassword = (password) => {
+  return bcrypt.hashSync(password, 10);
+};
+
+userSchema.methods.validatePassword = (password) => {
+  return bcrypt.compareSync(password, this.Password);
+};
 
 let User = mongoose.model("User", userSchema);
 
